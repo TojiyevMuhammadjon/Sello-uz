bc6f0aa8b3e3efbb156b944c39fd1a49ab03d516
Muhammadjon <132734174+TojiyevMuhammadjon@users.noreply.github.com>
2023-09-22 19:52:18 +0500

    Update password index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 const sequlize = new Sequelize(
-  "postgres://postgres:muhammad2959@localhost:5432/sello",
+  "postgres://postgres:postgres@localhost:5432/sello",
 );
 
 module.exports = sequlize;
